81fd555d9c9de93c33816888bff24655efe09adc
Monn Sena <raymonnsena@gmail.com>
2025-06-14 18:08:03 +0800

    added popular notes

--- notes/views.py
+++ notes/views.py
@@ -9,6 +9,12 @@ class NotesListView(ListView):
     context_object_name = "notes"
     template_name = "notes/notes_list.html"
 
+class PopularNotesListView(ListView):
+    model = Notes
+    context_object_name = "notes"
+    template_name = "notes/popular_notes.html"
+    queryset = Notes.objects.filter(likes__gte=1)
+
 class NotesDetailView(DetailView):
     model = Notes
     context_object_name = "notes"
